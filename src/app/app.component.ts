c29579d7bc7a4f2e7db792c606157fa4d511bcf1
Spyros Linardakis <s.linardakis@linakis.com>
2024-03-12 15:11:07 +0200

    update to call external api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
+
+  constructor(private http: HttpClient) {
+
+  }
   value = 'World';
+
+  doRequest() {
+    this.http.get("https://api.chucknorris.io/jokes/random").subscribe(x=>{
+      console.log(x);
+    });
+  }
 }
